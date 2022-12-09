3e2ee86ad0e48636d29bdab22a8e8041722b545d
alexglzg <alex_gg97@hotmail.com>
2022-12-09 10:36:09 +0100

    delatching when beginning

--- roboat_core/scripts/final_experiment.py
+++ roboat_core/scripts/final_experiment.py
@@ -220,8 +220,10 @@ def main():
     rospy.logwarn("Start")
     while (not rospy.is_shutdown()):
         if state == 0: #publish square shape to begin
+            t.latch_action(2)
             t.shape_publish(t.shape)
             time.sleep(1)
+            t.latch_action(2)
             t.shape_publish(t.shape)
             state = 1
         if state == 1: #configurate into a square
